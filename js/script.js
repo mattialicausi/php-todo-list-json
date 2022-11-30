48be3265d1a6517cc8e41b923e2d7b965541666a
mattialicausi <licausivitomattia@gmail.com>
2022-11-30 15:22:46 +0100

    preparato repo

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const { createApp } = Vue;
+
+const app = createApp({
+    data() {
+        return {
+           message: 'ciao',
+        }
+    },
+    methods: {
+        //METODI
+    },
+}).mount('#app')
